f4e6d21c3e0eae723574850a57a753ea9b8f7837
Luan-Diniz <luan.diniz.moraes@hotmail.com>
2022-07-12 11:14:02 -0300

    'separação de classes" Henrique

--- prototipo/main.py
+++ prototipo/main.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 import pygame
 from equipamentos import *
-from personagens import *
+from arquivos.personagens.player import *
+from arquivos.personagens.enemy import *
 from action import Disparo
 from map import *
 
