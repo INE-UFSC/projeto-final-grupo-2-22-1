1c36841c5f63b5ca9d0396153e10195d34cf4f6d
ClaudioGero <claudiogerolimetto@gmail.com>
2022-07-12 22:19:01 -0300

    Update botao.py

--- prototipo/botao.py
+++ prototipo/botao.py
@@ -4,17 +4,17 @@ import pygame
 
 class button():
     def __init__(self,color,x,y,width,height,text):
-        self.smallfont = pygame.font.SysFont('Corbel',30) 
+        self.smallfont = pygame.font.SysFont('Corbel',40) 
         self.width=width
         self.height=height
         self.rect=pygame.Rect((x,y),(width,height))
         self.color= color 
         self.x=x
         self.y=y
-        self.text = self.smallfont.render(text,True, '#00FF00')
+        self.text = self.smallfont.render(text,True, '#FFFFFF')
         self.text_rect=self.text.get_rect(center=self.rect.center)
     def draw(self,display):
-        pygame.draw.rect(display,'#FFFFFF',self.rect)
+        pygame.draw.rect(display,self.color,self.rect)
         display.blit(self.text,self.text_rect)
     def posicao(self,mouse):
         if mouse[0] > self.x and mouse[0] <self.x + self.width:
