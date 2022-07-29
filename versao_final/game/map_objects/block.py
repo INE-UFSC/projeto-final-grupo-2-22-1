924e53bf8669bdd38f34ce42fc1dcd3af8e1c818
unknown <luan.diniz.moraes@hotmail.com>
2022-07-28 21:21:06 -0300

    Tentativa de resolver incompatibilidades de OS

--- versao_final/game/map_objects/block.py
+++ versao_final/game/map_objects/block.py
@@ -2,9 +2,10 @@ import pygame
 class Block(pygame.sprite.Sprite):
     def __init__(self, pos, *groups):
         super().__init__(*groups)
-<<<<<<< HEAD
-        self.image = pygame.image.load('versao_final\game\image\Block\green_block.png').convert_alpha()  
-=======
-        self.image = pygame.image.load('versao_final\game\image\Block\green_block.png').convert_alpha()     
->>>>>>> becf2d8e06329a22a36180a8e6bfee29c30ef1c8
+        try:
+            self.image = pygame.image.load('versao_final\game\image\Block\green_block.png').convert_alpha()
+        except FileNotFoundError:
+            self.image = pygame.image.load('versao_final/game/image/Block/green_block.png').convert_alpha()
+
+
         self.rect = self.image.get_rect(topleft = pos)
