4aef220b4c9220d5273c8958260cb93d82284596
hpereira1 <pereirah123@gmail.com>
2022-07-28 21:28:25 -0300

    mudanças no door.py

--- versao_final/game/map_objects/door.py
+++ versao_final/game/map_objects/door.py
@@ -2,5 +2,9 @@ import pygame
 class Door(pygame.sprite.Sprite):
     def __init__(self, pos, *groups):
         super().__init__(*groups)
-        self.image = pygame.image.load("versao_finaldwgame\image\Object\door.png").convert_alpha()
+        try:
+            self.image = pygame.image.load('versao_final\game\image\Object\door.png').convert_alpha()
+        except FileNotFoundError:
+            self.image = pygame.image.load('versao_final/game/image/Object/door.png').convert_alpha()
+
         self.rect = self.image.get_rect(topleft = pos)
