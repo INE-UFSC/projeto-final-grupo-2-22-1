d5f8d0506938beb0395fa6ff051af4aad469e98a
hpereira1 <pereirah123@gmail.com>
2022-07-29 22:33:50 -0300

    enemy position

--- versao_final/game/entity/enemy.py
+++ versao_final/game/entity/enemy.py
@@ -1,8 +1,7 @@
-from turtle import pos
 import pygame
 import math
 from game.entity.character import Character
-
+import random
 class Enemy(Character):
     def __init__(self,pos, vida, dano,  *groups):
         super().__init__(vida, dano, *groups)
@@ -15,9 +14,9 @@ class Enemy(Character):
             self.image = pygame.image.load("versao_final\game\image\Enemy\enemy.png") 
         self.rect = self.image.get_rect(topleft = self.__pos)
         self.timer = 0
-
+        self.randpos = random.randint(200,400)
     def update(self,*args):
-        self.timer += 0.02
-        self.rect.x = self.__pos[0] + math.sin(self.timer)**3 * 320
-        self.rect.y = self.__pos[1] + math.cos(self.timer)**3 * 320
+        self.timer += 0.01
+        self.rect.x = self.__pos[0] + math.sin(self.timer)**3 * self.randpos
+        self.rect.y = self.__pos[1] + math.cos(self.timer)**3 * self.randpos
         
